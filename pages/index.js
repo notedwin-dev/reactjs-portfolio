18316ea0655536e44f92d7faf3d9c196f2c0cd5b
NotEdwin <notedwin.music@gmail.com>
2022-10-25 17:34:50 +0800

    added footer and footer css

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { BsCodeSlash, BsFillMoonStarsFill } from 'react-icons/bs';
 import { AiFillGithub, AiFillYoutube, AiFillLinkedin, AiFillHtml5 } from 'react-icons/ai';
-import { SiCsharp, SiDiscord, SiGit, SiHeroku, SiIntellijidea, SiJava, SiJavascript, SiNextdotjs, SiPython, SiReact, SiReplit, SiTailwindcss, SiTypescript, SiVisualstudiocode } from 'react-icons/si';
+import { SiCsharp, SiInstagram, SiDiscord, SiGit, SiHeroku, SiIntellijidea, SiJava, SiJavascript, SiNextdotjs, SiPython, SiReact, SiReplit, SiTailwindcss, SiTypescript, SiVisualstudiocode, SiYoutube, SiGithub } from 'react-icons/si';
 import { MdOutlineWeb } from 'react-icons/md';
 import { TbApi } from 'react-icons/tb';
 import { BiCodeBlock } from 'react-icons/bi';
@@ -16,6 +16,7 @@ import acode_djs from '../public/autocode-discordjs.png';
 
 
 export default function Home() {
+  let cards = "text-center shadow-xl bg-gradient-to-b from-sky-100 to-slate-100 shadow-sky-200 p-10 rounded-xl my-10 dark:shadow-sky-700 dark:from-sky-200 dark:to-slate-200"
   let tag = "text-sm inline-flex items-center font-sans font-semibold leading-sm px-3 py-1 bg-slate-100 text-sky-500 rounded-full m-1 border-lg border-2 border-blue-400"
 
   const [darkMode, setDarkMode] = useState(false);
@@ -28,8 +29,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10 dark:bg-slate-800">
-        <section className="min-h-screen">
+      <main className="bg-white dark:bg-slate-800">
+        <section className="min-h-screen px-10">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons dark:text-white">Edwin Ng</h1>
             <ul className="flex items-center">
@@ -45,8 +46,8 @@ export default function Home() {
             <Image src={me} className="mx-auto rounded-full w-80 h-80" alt='' />
           </div>
           <div className="text-center p-10">
-            <h2 className="font-sans text-5xl py-2 text-blue-gray">Edwin Ng</h2>
-            <h3 className="font-sans text-2xl py-2 dark:text-white">Back-End Developer and University Student.</h3>
+            <h2 className="font-sans text-5xl py-2 text-blue-gray-100 dark:text-cyan-500">Edwin Ng</h2>
+            <h3 className="font-sans text-2xl py-2 text-black dark:text-white">Back-End Developer and University Student.</h3>
             <p className="font-sans leading-8 text-gray-600 dark:text-gray-300">Currently working on multiple Github Repositories such as discord-ws and autocode-discordjs.</p>
           </div>
           <div className="text-3xl flex justify-center gap-10">
@@ -60,15 +61,15 @@ export default function Home() {
           </div>
         </section>
 
-        <section>
+        <section className='px-10'>
           <div className="text-center">
-            <h2 className="font-sans font-semibold text-3xl py-2 text-blue-gray dark:text-cyan-500">Skills</h2>
+            <h2 id="skills" className="font-sans font-semibold text-3xl py-2 text-blue-gray-100 dark:text-cyan-500">Skills</h2>
             <p className="font-sans text-md leading-8 text-gray-600 dark:text-gray-300">Here are some of the <span className="text-blue-gray dark:text-cyan-500">Skills</span>, <span className="text-blue-gray dark:text-cyan-500">Programming Languages</span> and <span className="text-blue-gray dark:text-cyan-500">Tools</span> that I am familiar with.</p>
           </div>
 
           <div className="lg:flex gap-8">
 
-            <div className="text-center shadow-xl bg-gradient-to-b from-sky-100 to-slate-100 shadow-sky-200 p-10 rounded-xl my-10 dark:shadow-sky-700 dark:from-sky-200 dark:to-slate-200">
+            <div className={cards}>
               <IoLogoNodejs className="mx-auto text-5xl text-green-500" />
               <h2 className="font-bold text-blue-400 dark:text-green-600">Node.js</h2>
               <p>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment.</p>
@@ -91,7 +92,7 @@ export default function Home() {
               <div className={tag}><SiJavascript className='mr-1' /> axios</div>
             </div>
 
-            <div className="text-center shadow-xl bg-gradient-to-b from-sky-100 to-slate-100 shadow-sky-200 p-10 rounded-xl my-10 dark:shadow-sky-700 dark:from-sky-200 dark:to-slate-200">
+            <div className={cards}>
               <BsCodeSlash className="mx-auto text-5xl text-blue-600" />
               <h2 className="font-bold text-blue-400 dark:text-blue-700">Progamming Languages</h2>
               <p>A programming language is a system of notation for writing computer programs.</p>
@@ -111,7 +112,7 @@ export default function Home() {
               <div className={tag}><SiTypescript className='mr-1' /> TypeScript</div>
             </div>
 
-            <div className="text-center shadow-xl bg-gradient-to-b from-sky-100 to-slate-100 shadow-sky-200 p-10 rounded-xl my-10 dark:shadow-sky-700 dark:from-sky-200 dark:to-slate-200">
+            <div className={cards}>
               <MdOutlineWeb className="mx-auto text-5xl text-yellow-500" />
               <h2 className="font-bold text-blue-400 dark:text-yellow-600">Web Development</h2>
               <p>Web development is the work involved in developing a website for the Internet</p>
@@ -134,39 +135,117 @@ export default function Home() {
           </div>
         </section>
 
-        <section>
+        <section className='px-10'>
           <div className="text-center">
-            <h2 className="font-sans font-semibold text-3xl py-2 text-blue-gray dark:text-cyan-500">Projects</h2>
-            <p className="font-sans text-md leading-8 text-gray-600 dark:text-gray-300">Here are some of the <span className="text-blue-gray dark:text-cyan-500">projects</span> that I have built.</p>
+            <h2 id="projects" className="font-sans font-semibold text-3xl py-2 text-blue-gray-100 dark:text-cyan-500">Projects</h2>
+            <p className="font-sans text-md leading-8 text-gray-600 dark:text-gray-300">Here are some of the <span className="text-blue-gray-100 dark:text-cyan-500">projects</span> that I have built.</p>
           </div>
           <div>
             <div className="grid gap-10 mb-10 md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 ">
-              <a href="https://notedwin.autocode.dev/discord-bot-dashboard@dev/">
-                <Image src={dashboard1} alt='' className="rounded-xl" />
-                <h3 className="font-bold text-center dark:text-cyan-500"> Discord Bot Dashboard </h3>
+              <div id="dashboard">
+                <a href="https://notedwin.autocode.dev/discord-bot-dashboard@dev/">
+                  <Image src={dashboard1} alt='' className="rounded-xl" />
+                </a>
+                <h3 className="font-bold text-center text-blue-gray-100 dark:text-cyan-500"> Discord Bot Dashboard </h3>
                 <p className="text-center dark:text-white">A Discord Bot Dashboard Web Application created using Autocode.</p>
-              </a>
-              <a href="https://notedwin.co">
-                <Image src={portfolio} alt='' className="rounded-xl"></Image>
-                <h3 className="font-bold text-center dark:text-cyan-500"> Portfolio Website </h3>
+
+                <div>
+                  <h1 className="text-xl inline-flex text-cyan-500">Tags:</h1>
+                  <div className={tag}>Node.js</div>
+                  <div className={tag}>lib</div>
+                  <div className={tag}>MongoDB</div>
+                  <div className={tag}>HTML</div>
+                  <div className={tag}>CSS</div>
+                  <div className={tag}>JavaScript</div>
+                </div>
+
+              </div>
+              <div id="portfolio">
+                <a href="https://notedwin.co">
+                  <Image src={portfolio} alt='' className="rounded-xl"></Image>
+                </a>
+                <h3 className="font-bold text-center text-blue-gray-100 dark:text-cyan-500"> Portfolio Website </h3>
                 <p className="text-center dark:text-white">A Portfolio Website Template created as beginners tutorial on Autocode.</p>
-              </a>
-              <a href='https://www.github.com/Edwin15857/discord-ws'>
-                <Image src={ws} alt='' className="rounded-xl"></Image>
-                <h3 className="font-bold text-center dark:text-cyan-500"> Discord WebSocket </h3>
+
+                <div>
+                  <h1 className="text-xl inline-flex text-cyan-500">Tags:</h1>
+                  <div className={tag}>HTML</div>
+                  <div className={tag}>CSS</div>
+                  <div className={tag}>JavaScript</div>
+                </div>
+
+              </div>
+              <div id="websocket">
+                <a href='https://www.github.com/Edwin15857/discord-ws'>
+                  <Image src={ws} alt='' className="rounded-xl"></Image>
+                </a>
+                <h3 className="font-bold text-center text-blue-gray-100 dark:text-cyan-500"> Discord WebSocket </h3>
                 <p className="text-center dark:text-white">Allows developers to easily implement Discord Gateway without worrying about the backend.</p>
-              </a>
-              <a href='https://www.github.com/Edwin15857/autocode-discordjs'>
-                <Image src={acode_djs} alt='' className="rounded-xl"></Image>
-                <h3 className="font-bold text-center dark:text-cyan-500"> Autocode Discord.js </h3>
+
+                <div>
+                  <h1 className="text-xl inline-flex text-cyan-500">Tags:</h1>
+                  <div className={tag}>Node.js</div>
+                  <div className={tag}>JSON</div>
+                  <div className={tag}>ws</div>
+                </div>
+
+              </div>
+              <div id="discord.js">
+                <a href='https://www.github.com/Edwin15857/autocode-discordjs'>
+                  <Image src={acode_djs} alt='' className="rounded-xl"></Image>
+                </a>
+                <h3 className="font-bold text-center text-blue-gray-100 dark:text-cyan-500"> Autocode Discord.js </h3>
                 <p className="text-center dark:text-white">Created to make support for discord.js possible on Autocode&apos;s serverless environment.</p>
-              </a>
+
+                <div>
+                  <h1 className="text-xl inline-flex text-cyan-500">Tags:</h1>
+                  <div className={tag}>Node.js</div>
+                  <div className={tag}>lib</div>
+                </div>
+
+              </div>
             </div >
           </div >
         </section >
-        <footer className="flex justify-left gap-10">
-          <a href="mailto:me@notedwin.co" className=" dark:text-white">Contact</a>
-          <a href="#" className="dark:text-white">About Me</a>
+        <footer className="h-40">
+          <div class="  rounded-xl items-center flex justify-center h-40">
+            <div className="flex flex-wrap m-15">
+              <div class="flex-col relative m-5">
+                <h4 className="font-sans font-bold text-xl">Quick Links</h4>
+                <ul class="list-none text-sm">
+                  <li class="footer-link-item"><a class="footer-link" href="https://notedwin.co">Portfolio</a></li>
+                  <li class="footer-link-item"><a class="footer-link" href="https://fiverr.com/edwin_2004">Services</a></li>
+                </ul>
+              </div>
+              <div class="flex-col relative m-5">
+                <h4 className="font-sans font-bold text-xl">Developers</h4>
+                <ul class="list-none text-sm">
+                  <li class="footer-link-item"><a class="footer-link" href="https://github.com/Edwin15857">GitHub</a></li>
+
+                </ul>
+              </div>
+              <div className="flex-col relative m-5">
+                <h4 className="font-sans font-bold text-xl">Legal</h4>
+                <ul class="list-none text-sm">
+                  <li class="footer-link-item"><a class="footer-link" href="https://notedwin.co/privacy">Privacy Policy</a></li>
+                  <li class="footer-link-item"><a class="footer-link" href="mailto:privacy@notedwin.co">Privacy Concerns</a></li>
+                </ul>
+              </div>
+              <div class="flex-col relative m-5">
+                <h4 className="font-sans font-bold text-xl">Contact</h4>
+                <ul class="list-none text-sm">
+                  <li class="footer-link-item"><a class="footer-link" href="mailto:info@notedwin.co">info@notedwin.co</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="justify-center">
+              <a className="mx-5 mb-5" href="https://youtube.com/@NotEdwinOfficial"><SiYoutube /></a>
+
+              <a className="mx-5 mb-5" href="https://instagram.com/edwin.ngggggggg"><SiInstagram /></a>
+
+              <a className="mx-5 mb-5" href="https://github.com/Edwin15857"><SiGithub /></a>
+            </div>
+          </div>
         </footer>
       </main >
     </div >
