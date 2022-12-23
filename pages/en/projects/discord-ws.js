9229427db8dd94ecfead0e061ce5da642e50af35
NotEdwin <notedwin.music@gmail.com>
2022-12-23 09:33:07 +0800

    further improve discord-ws elaboration

--- pages/en/projects/discord-ws.js
+++ pages/en/projects/discord-ws.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import Nav from '../../../components/nav';
 import Footer from '../../../components/footer';
+import ws from '../../../public/discord-websocket.svg'
 
 export default function DiscordWS() {
     const textBox = "font-bold p-1 border-2 rounded-md fill-white"
@@ -19,13 +21,17 @@ export default function DiscordWS() {
                 <section className="px-10">
                     <Nav icons="Project" />
                     <div className="text-center mx-10">
-                        <h1 className="font-bold text-2xl text-slate-500 pb-5">
+                        <h1 className="font-bold text-2xl text-slate-500 pb-5 dark:text-blue-gray-100">
                             Discord WebSocket
                         </h1>
                         <p className="text-md mb-16 dark:text-white">
-                            In this project, I managed to implement a Discord WebSocket Gateway that are used for receiving Discord events such as <code className={textBox}>MESSAGE_CREATE</code>, <code className={textBox}>VOICE_STATE_UPDATE</code>, <code className={textBox}>GUILD_MEMBER_ADD</code>, and more.
+                            In this project, I managed to implement a Discord WebSocket Gateway that are used for receiving Discord events such as <code className={textBox}>MESSAGE_CREATE</code>, <code className={textBox}>VOICE_STATE_UPDATE</code>, <code className={textBox}>GUILD_MEMBER_ADD</code>, and more. Every events sent by the Discord Gateway is actually the most crucial feature to Discord libraries such as Discord.js, Discord.py and PyCord itself. Without these events received from the gateway, these modules will be useless, just like an empty shell. I have learnt a lot about WebSocket from this project and realised a bigger potential of this project. It shouldn't just be a simple Discord WebSocket project. It can be the base of many easy-to-use Discord libraries built by novice coders! This is why I recommend novices who are trying to build a new Discord library to use my Github project as the base of their very own Discord Library. Not only it will reduce the amount of work you have to do to create the library, it will also save you a bunch of time without needing you to learn and research about how to connect to the Discord&apos;s Gateway! This will definitely help you complete your dream of creating your very own Discord library as a novice coder in no time~ Of course if you don't like to fork the entire project of other people&apos;s work, you can also use my Github repository as reference to help you learn how to connect to the Discord Gateway!
                         </p>
-                        <Link href="https://github.com/Edwin15857/discord-ws"><a><h1 className="font-bold text-2xl text-slate-500 pb-5">Discord WebSocket Github Repository</h1></a></Link>
+                        <div className='mb-3'>
+                            <Link href="https://github.com/Edwin15857/discord-ws"><a><h1 className="font-bold text-2xl text-slate-500 pb-2 dark:text-blue-gray-100">Discord WebSocket Github Repository</h1></a></Link>
+
+                            <Image src={ws} alt='' className="bg-slate-700 dark:bg-transparent"></Image>
+                        </div>
                     </div>
 
                 </section>
--- pages/zh/projects/discord-ws.js
+++ pages/zh/projects/discord-ws.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import Nav from '../../../components/zh/nav';
-import Footer from '../../../components/zh/footer';
+import Nav from '../../../components/nav';
+import Footer from '../../../components/footer';
+import Image from 'next/image';
+import ws from '../../../public/discord-websocket.svg'
 
 export default function DiscordWS() {
+    const textBox = "font-bold p-1 border-2 rounded-md fill-white"
     return (
         <div>
             <Head>
@@ -18,18 +21,22 @@ export default function DiscordWS() {
                 <section className="px-10">
                     <Nav icons="Project" />
                     <div className="text-center mx-10">
-                        <h1 className="font-bold text-2xl text-slate-500 pb-5">
+                        <h1 className="font-bold text-2xl text-slate-500 pb-5 dark:text-blue-gray-100">
                             Discord WebSocket
                         </h1>
                         <p className="text-md mb-16 dark:text-white">
-                            In this project, I managed to implement a Discord WebSocket Gateway that are used for receiving Discord events such as <code>MESSAGE_CREATE</code>.
+                            在这个项目里，我成功连接了 Discord 的 WebSocket 网关。这个网关可以接受到Discord的事件发射器所发送的 <code className={textBox}>MESSAGE_CREATE</code>, <code className={textBox}>VOICE_STATE_UPDATE</code>, <code className={textBox}>GUILD_MEMBER_ADD</code> 等等。Discord网关所发送的事件其实是所有大小型的Discord模块（例如Discord.js，Discord.py及PyCord）本身最关键的功能。少了Discord网关发送的这些Discord事件，这些模块就会像是空壳一样毫无作用。本项目让我学到了很多关于WebSocket的知识并且让我很快的意识到了这个项目其实有着一股更大的潜力。他不该只是简单的Discord WebSocket项目。它是一个可以帮助新手创造更多简单易用的Discord模块的基础项目！因此我强力推荐想要自创Discord模块的新手们拿我的GitHub项目来作为模块的基础。这样以来，既可以减少大部分的工作，亦可以不用学习如何使用 Discord 的 WebSocket 网关。帮助你早日完成你一直心心念念的Discord模块~ 如果不想拿完整的项目照抄的话，你也可以以我的GitHub项目作为参考，以协助你完成WebSocket的连接部分。
                         </p>
-                        <Link href="https://github.com/Edwin15857/discord-ws"><a><h1 className="font-bold text-2xl text-slate-500 pb-5">Discord WebSocket Github Repository</h1></a></Link>
+                        <div className='mb-3'>
+                            <Link href="https://github.com/Edwin15857/discord-ws"><a><h1 className="font-bold text-2xl text-slate-500 pb-2">Discord WebSocket Github Repository</h1></a></Link>
+
+                            <Image src={ws} alt=''></Image>
+                        </div>
                     </div>
 
                 </section>
                 <Footer />
             </main>
-        </div>
+        </div >
     )
 }
