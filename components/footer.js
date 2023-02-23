9fcb8284483218c683891959475c1d9cd40e0510
NotEdwin <notedwin.music@gmail.com>
2023-02-23 19:45:45 +0800

    fixed a into link

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { SiInstagram, SiYoutube, SiGithub } from 'react-icons/si'
 
 export default function Footer() {
@@ -9,43 +10,43 @@ export default function Footer() {
                     <div class="flex-col relative m-5">
                         <h4 className="font-sans font-bold text-xl dark:text-cyan-500">Quick Links</h4>
                         <ul className="list-none text-sm">
-                            <li className="dark:text-white"><a href="/en/projects">Project Directory</a></li>
-                            <li className="dark:text-white"><a href="https://notedwin.co" target="blank">Portfolio Website</a></li>
-                            <li className="dark:text-white"><a href="https://fiverr.com/edwin_2004" target="blank">Fiverr</a></li>
+                            <li className="dark:text-white"><Link href="/en/projects">Project Directory</Link></li>
+                            <li className="dark:text-white"><Link href="https://notedwin.co" target="blank">Portfolio Website</Link></li>
+                            <li className="dark:text-white"><Link href="https://fiverr.com/edwin_2004" target="blank">Fiverr</Link></li>
                         </ul>
                     </div>
                     <div className="flex-col relative m-5">
                         <h4 className="font-sans font-bold text-xl dark:text-cyan-500">Development</h4>
                         <ul className="list-none text-sm">
-                            <li className="dark:text-white"><a href="https://github.com/Edwin15857" target="blank">GitHub</a></li>
-                            <li className="dark:text-white"><a href="https://link.notedwin.co/tutorials-youtube" target="blank">YouTube</a></li>
-                            <li className='dark:text-white'><a href="https://autocode.com/notedwin/snippets/" target="blank">Autocode Snippets</a></li>
-                            <li className='dark:text-white'><a href="https://autocode.com/notedwin/template/" target="blank">Autocode Templates</a></li>
+                            <li className="dark:text-white"><Link href="https://github.com/Edwin15857" target="blank">GitHub</Link></li>
+                            <li className="dark:text-white"><Link href="https://link.notedwin.co/tutorials-youtube" target="blank">YouTube</Link></li>
+                            <li className='dark:text-white'><Link href="https://autocode.com/notedwin/snippets/" target="blank">Autocode Snippets</Link></li>
+                            <li className='dark:text-white'><Link href="https://autocode.com/notedwin/template/" target="blank">Autocode Templates</Link></li>
                         </ul>
                     </div>
                     <div className="flex-col relative m-5">
                         <h4 className="font-sans font-bold text-xl dark:text-cyan-500">Legal</h4>
                         <ul className="list-none text-sm">
-                            <li className="dark:text-white"><a href="https://notedwin.co/privacy-policy" target="blank">Privacy Policy</a></li>
-                            <li className="dark:text-white"><a href="https://notedwin.co/terms-and-conditions" target="blank">Terms and Conditions</a></li>
-                            <li className="dark:text-white"><a href="mailto:privacy@notedwin.co">Privacy Concerns</a></li>
+                            <li className="dark:text-white"><Link href="https://notedwin.co/privacy-policy" target="blank">Privacy Policy</Link></li>
+                            <li className="dark:text-white"><Link href="https://notedwin.co/terms-and-conditions" target="blank">Terms and Conditions</Link></li>
+                            <li className="dark:text-white"><Link href="mailto:privacy@notedwin.co">Privacy Concerns</Link></li>
                         </ul>
                     </div>
                     <div className="flex-col relative m-5">
                         <h4 className="font-sans font-bold text-xl dark:text-cyan-500">Contact</h4>
                         <ul className="list-none text-sm">
-                            <li className="dark:text-white"><a href="mailto:info@notedwin.co">info@notedwin.co</a></li>
-                            <li className="dark:text-white"><a href="mailto:collabs@notedwin.co">collabs@notedwin.co</a></li>
-                            <li className='dark:text-white'><a href="https://discordapp.com/users/">Discord DMs</a></li>
+                            <li className="dark:text-white"><Link href="mailto:info@notedwin.co">info@notedwin.co</Link></li>
+                            <li className="dark:text-white"><Link href="mailto:collabs@notedwin.co">collabs@notedwin.co</Link></li>
+                            <li className='dark:text-white'><Link href="https://discordapp.com/users/">Discord DMs</Link></li>
                         </ul>
                     </div>
                 </div>
                 <div className="justify-center">
-                    <a className="mx-5 mb-5 dark:text-white" href="https://youtube.com/@NotEdwinOfficial" target="blank"><SiYoutube /></a>
+                    <Link className="mx-5 mb-5 dark:text-white" href="https://youtube.com/@NotEdwinOfficial" target="blank"><SiYoutube /></Link>
 
-                    <a className="mx-5 mb-5 dark:text-white" href="https://instagram.com/edwin.ngggggggg" target="blank"><SiInstagram /></a>
+                    <Link className="mx-5 mb-5 dark:text-white" href="https://instagram.com/edwin.ngggggggg" target="blank"><SiInstagram /></Link>
 
-                    <a className="mx-5 mb-5 dark:text-white" href="https://github.com/Edwin15857" target="blank"><SiGithub /></a>
+                    <Link className="mx-5 mb-5 dark:text-white" href="https://github.com/Edwin15857" target="blank"><SiGithub /></Link>
                 </div>
             </div>
         </footer>
